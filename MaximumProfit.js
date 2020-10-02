3d98c139234c59a91ee7a5538aa9cf2d3217eece
Josh <jolumese@live.com>
2020-10-01 21:36:22 -0400

    MaximumProfit
    MaximumProfit Problem solved

--- MaximumProfit.js
+++ MaximumProfit.js
@@ -1,6 +1,15 @@
 class MaximumProfit {
     find(input){
-        return null;
+    	let maxProfit = 0;
+    	let left = 0;
+    	for (let right = 1; right < input.length; right++) {
+    		if (input[left] > input[right]) {
+    			left = right;
+    		} else if (input[left] < input[right] && input[right] - input[left] > maxProfit) {
+    			maxProfit = input[right] - input[left];
+    		}
+    	}
+        return maxProfit;
     }
 }
 
